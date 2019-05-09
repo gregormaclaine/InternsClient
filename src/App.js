c5f1913d219d07e1696d042f43e15160c7672351
Gregor Maclaine <thegregormaclaine@gmail.com>
2019-05-09 16:57:56 +0100

    Revert "renamed page files"
    This reverts commit ea7fe359cb491ba39c73e4caed5163fd4b2acaa5.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import axios from "axios";
+import CoursesPage from './pages/CoursesPage';
+import CoursePage from './pages/CoursePage';
+import IndexPage from './pages/IndexPage';
+import FAQPage from './pages/FaqPage';
 import {Header, Container, Main, Footer} from "./components/Elements";
-import IndexPage from './pages/Home';
-import CoursesPage from './pages/Levels';
-import CoursePage from './pages/Course';
-import FAQPage from './pages/FAQ';
+import axios from "axios";
 
 export const CourseContext = React.createContext();
 
